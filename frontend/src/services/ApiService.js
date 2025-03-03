5d7a062544b6c9150710f6dace68351e3bcd241a
apandit646 <118040831+apandit646@users.noreply.github.com>
2025-03-03 13:05:53 +0530

    Merge branch 'main' of https://github.com/Nikunjjjj/society-management

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -2,24 +2,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const someApi = createApi({
   reducerPath: 'someApi',
-<<<<<<< Updated upstream
-  baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:5000/' 
-=======
-<<<<<<< Updated upstream
-  baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:5000/' 
-=======
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:6000"
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+    baseUrl: 'http://localhost:5000/'
   }),
-
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (userData) => ({
-        url: '/signup',
+        url: 'signup',
         method: 'POST',
         body: userData,
       }),
