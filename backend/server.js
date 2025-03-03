31d7a0e63430a5b4add4737137242bab012f464d
Nikunj <nikunjx999@gmail.com>
2025-03-03 13:00:55 +0530

    login and signup completed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const signupRouter = require('./routers/signupRouter');
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.json());
