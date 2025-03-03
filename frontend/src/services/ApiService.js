5554c7c55caec0276e9ef964e8f3cfd920804239
Nikunj <nikunjx999@gmail.com>
2025-03-03 13:10:03 +0530

    login and signup completed again

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -3,20 +3,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const someApi = createApi({
   reducerPath: 'someApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:5000/' 
+    baseUrl: 'http://localhost:5000' 
   }),
 
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (userData) => ({
-        url: 'signup',
+        url: '/signup',
         method: 'POST',
         body: userData,
       }),
     }),
     login: builder.mutation({
       query: (credentials) => ({
-        url: 'login',
+        url: '/login',
         method: 'POST',
         body: credentials,
       }),
