b6908ee0218580f17ee306bc253ec5778268c4bd
Nikunj <nikunjx999@gmail.com>
2025-03-18 16:47:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,27 @@ import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/SignUp/Signup";
 import EmailVerification from "./pages/EmailVerfication";
-import Dashboard from "./pages/Dashboard/Dashboard"
+import Dashboard from "./pages/Dashboard/Dashboard";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const token = localStorage.getItem("token");
+
   return (
     <div>
-       <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="top-right" autoClose={3000} />
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/emailverification" element={<EmailVerification />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {token ? (
+            <Route path="/dashboard" element={<Dashboard />} />
+          ) : (
+            <Route path="/signup" element={<SignUp />} />
+          )}
         </Routes>
       </BrowserRouter>
     </div>
